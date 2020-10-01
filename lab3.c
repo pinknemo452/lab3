d96aa0f4eda7de65c7059866d4b7bd122df8d41c
pinknemo452 <mishanay38@gmail.com>
2020-10-01 16:45:19 +0300

    first lab3 commit

---
+++ lab3.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+void sort_array(int* arr, int n) {
+	int temp;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if ((*arr < 0) ) {
+				arr++;
+				if (*arr > 0) {
+					temp = *arr;
+					arr--;
+					int temp1 = *arr;
+					*arr = temp;
+					arr++;
+					*arr = temp1;
+				}
+			}
+			else {
+				arr++;				
+			}
+		}
+		arr -= n;
+	}
+}
+int main() {
+	int array[7] = { 1,-1,-2,-3,5,4,6 };
+	sort_array(array,7 );
+	for (int j = 0; j < 7; j++) {
+		printf("%d ", array[j]);
+	}
+	return 0;
+}
