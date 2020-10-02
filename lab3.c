f774786557ef9164db22e6b672ec3fdc1de35804
pinknemo452 <mishanay38@gmail.com>
2020-10-02 14:55:20 +0300

    fix 0 bug

--- lab3.c
+++ lab3.c
@@ -3,9 +3,9 @@ void sort_array(int* arr, int n) {
 	int temp;
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) {
-			if ((*arr < 0) ) {
+			if ((*arr <= 0) ) {
 				arr++;
-				if (*arr > 0) {
+				if (*arr >= 0) {
 					temp = *arr;
 					arr--;
 					int temp1 = *arr;
@@ -22,7 +22,7 @@ void sort_array(int* arr, int n) {
 	}
 }
 int main() {
-	int array[7] = { 1,-1,-2,-3,5,4,6 };
+	int array[7] = { 1,-1,-2,-3,5,4,0 };
 	sort_array(array,7 );
 	for (int j = 0; j < 7; j++) {
 		printf("%d ", array[j]);
