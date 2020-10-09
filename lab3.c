6d18a48fadaf9b0ced69edad7eeb59c83e95b218
pinknemo452 <mishanay38@gmail.com>
2020-10-09 15:34:31 +0300

    dop stable 0.1 commit

--- lab3.c
+++ lab3.c
@@ -1,9 +1,17 @@
 #include<stdio.h>
-void sort_array(int* arr, int n) {
+int predicate(int a, int b) {
+	if (a > b) {
+		return a;
+	}
+	else  {
+		return b;
+	}
+}
+void sort_array(int* arr, int n,int (*predicate)(int)) {
 	int x;
 	for (int i = 1; i <= n - 1; i++) {
 		for (int j = 0; j < n - i; j++) {
-			if (*(arr + j) > * (arr + j + 1)) {
+			if (predicate(*(arr + j), *(arr + j + 1)) == *(arr + j)/**(arr + j) > * (arr + j + 1)*/) {
 				x = *(arr + j);
 				*(arr + j) = *(arr + j + 1);
 				*(arr + j + 1) = x;
@@ -19,7 +27,7 @@ int main() {
 		printf("A[%d] = ", i);
 		scanf("%d", &array1[i]);
 	}
-	sort_array(array1, n );
+	sort_array(array1, n, predicate );
 	printf("\n");
 	for (int j = n-1; j >=0; j--) {
 		printf("%d ", array1[j]);
