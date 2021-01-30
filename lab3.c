f3ea9c9c1afe9ac148249a3db54506778eb4c7c2
Yarmolovich Mikhail <mishanay38@gmail.com>
2021-01-30 12:37:00 +0300

    #1

--- lab3.c
+++ lab3.c
@@ -1,20 +1,24 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <malloc.h>
 void sort_array(int* arr, int n) {
 	int x;
 	for (int i = 1; i <= n - 1; i++) {
 		for (int j = 0; j < n - i; j++) {
-			if (*(arr + j) > * (arr + j + 1)) {
-				x = *(arr + j);
-				*(arr + j) = *(arr + j + 1);
-				*(arr + j + 1) = x;
+			if (arr[j] > arr[j+1]) {
+				x = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = x;
 			}
 		}
 	}
 }
 int main() {
-	int array1[999],n;
+	int n = 0;
 	printf("enter n ");
-	scanf("%d", &n);
+	while (n <= 0) {
+		scanf("%d", &n);
+	}
+	int* array1 = (int*)malloc(n * sizeof(int));
 	for (int i = 0; i < n; i++) {
 		printf("A[%d] = ", i);
 		scanf("%d", &array1[i]);
